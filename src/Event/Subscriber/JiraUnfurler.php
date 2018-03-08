6f7724dc4de3966279299817b28e67611f344e7d
Elan Ruusamäe <glen@delfi.ee>
2018-03-08 21:00:21 +0200

    trivial title expanding for jira

--- src/Event/Subscriber/JiraUnfurler.php
+++ src/Event/Subscriber/JiraUnfurler.php
@@ -2,6 +2,7 @@
 
 namespace JiraSlackUnfurl\Event\Subscriber;
 
+use chobie\Jira;
 use Psr\Log\LoggerInterface;
 use SlackUnfurl\Event\Events;
 use SlackUnfurl\Event\UnfurlEvent;
@@ -12,13 +13,17 @@ class JiraUnfurler implements EventSubscriberInterface
 {
     use LoggerTrait;
 
+    /** @var Jira\Api */
+    private $api;
     /** @var string */
     private $domain;
 
     public function __construct(
+        Jira\Api $api,
         string $domain,
         LoggerInterface $logger
     ) {
+        $this->api = $api;
         $this->domain = $domain;
         $this->logger = $logger;
     }
@@ -36,9 +41,35 @@ class JiraUnfurler implements EventSubscriberInterface
     public function unfurl(UnfurlEvent $event)
     {
         foreach ($event->getMatchingLinks($this->domain) as $link) {
-            $url = $link['url'];
-            $unfurl = [];
-            $event->addUnfurl($url, $unfurl);
+            $unfurl = $this->getIssueUnfurl($link['url']);
+            if ($unfurl) {
+                $event->addUnfurl($link['url'], $unfurl);
+            }
         }
+    }
+
+    private function getIssueUnfurl(string $url)
+    {
+        $issue = $this->getIssueDetails($url);
+        $this->debug('jira', ['issue' => $issue]);
+
+        if (!$issue) {
+            return null;
+        }
+
+        return [
+            'title' => "<$url|#{$issue['key']}>: {$issue['fields']['summary']}",
+        ];
+    }
+
+    private function getIssueDetails(string $url)
+    {
+        if (!preg_match("#^https?://\Q{$this->domain}\E/browse/(?P<projectKey>[\d\w]+)-(?P<issueId>\d+)#", $url, $m)) {
+            return null;
+        }
+
+        $issueKey = "{$m['projectKey']}-{$m['issueId']}";
+
+        return $this->api->getIssue($issueKey)->getResult();
     }
 }
--- src/ServiceProvider/JiraUnfurlServiceProvider.php
+++ src/ServiceProvider/JiraUnfurlServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace JiraSlackUnfurl\ServiceProvider;
 
+use chobie\Jira;
 use JiraSlackUnfurl\Event\Subscriber\JiraUnfurler;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
@@ -16,13 +17,23 @@ class JiraUnfurlServiceProvider implements ServiceProviderInterface, EventListen
     public function register(Container $app)
     {
         $app['jira.url'] = getenv('JIRA_URL');
+        $app['jira.username'] = getenv('JIRA_USERNAME');
+        $app['jira.password'] = getenv('JIRA_PASSWORD');
 
         $app[JiraUnfurler::class] = function ($app) {
             $domain = parse_url($app['jira.url'], PHP_URL_HOST);
 
             return new JiraUnfurler(
+                $app[Jira\Api::class],
                 $domain,
                 $app['logger']
+            );
+        };
+
+        $app[Jira\Api::class] = function ($app) {
+            return new Jira\Api(
+                $app['jira.url'],
+                new Jira\Api\Authentication\Basic($app['jira.username'], $app['jira.password'])
             );
         };
     }
